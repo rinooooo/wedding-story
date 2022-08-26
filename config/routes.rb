3b690278f32576bd233bc0bad40117a9cdf94134
rinooooo <rino.0566@gmail.com>
2022-08-26 20:09:00 +0900

    postモデルとuserモデルのアソシエーション

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
+
   devise_scope :user do
     get 'weddings', to: 'users/registrations#new_wedding'
     post 'weddings', to: 'users/registrations#create_wedding'
+  end
+
+  resource :users do
+    resources :posts, only: [:index]
   end
   root to: "posts#index"
 end
--- db/migrate/20221111111111_create_posts.rb
+++ db/migrate/20221111111111_create_posts.rb
@@ -4,6 +4,7 @@ class CreatePosts < ActiveRecord::Migration[6.0]
       t.string     :name,                null: false
       t.text       :message,             null: false
       t.integer    :attendance_id,       null: false
+      t.references :user, foreign_key: true, null: false
       t.timestamps
     end
   end
