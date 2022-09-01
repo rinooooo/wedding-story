6679ff99961864a41c4cef58ce672275408c6a65
rinooooo <rino.0566@gmail.com>
2022-09-01 19:44:59 +0900

    式情報編集機能の実装

--- app/controllers/weddings_controller.rb
+++ app/controllers/weddings_controller.rb
@@ -1,10 +1,24 @@
 class WeddingsController < ApplicationController
   def edit
     @user = User.find(params[:user_id])
-    @wedding = Wedding.find(params[:id])
+    @wedding = @user.wedding
   end
 
   def update
-    @wedding = Wedding.find(params[:id])
+    @user = User.find(params[:user_id])
+    @wedding = @user.wedding
+    binding.pry
+    if @wedding.valid?
+      @wedding.update(wedding_params)
+      redirect_to user_posts_path(@user.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+ 
+  def wedding_params
+    params.require(:wedding).permit(:gloom_name, :bride_name, :gloom_name_en, :bride_name_en, :date, :answer_date, :message, :message_info, :location, :url, :address, :reception_time, :wedding_time, :photo_time, :party_time, :bye_time, :second_party_time)
   end
 end
