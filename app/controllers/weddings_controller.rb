ddc8f8f8070591d464a4aafb748a2c30f95b6720
rinooooo <rino.0566@gmail.com>
2022-09-01 19:24:41 +0900

    編集ページの編集

--- app/controllers/weddings_controller.rb
+++ app/controllers/weddings_controller.rb
@@ -1,7 +1,10 @@
 class WeddingsController < ApplicationController
   def edit
+    @user = User.find(params[:user_id])
+    @wedding = Wedding.find(params[:id])
   end
 
   def update
+    @wedding = Wedding.find(params[:id])
   end
 end
