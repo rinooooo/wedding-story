83bc8d9bc91efae0b77be918977d30df8a127115
rinooooo <rino.0566@gmail.com>
2022-09-10 21:54:49 +0900

    ユーザーログイン後に遷移するページの変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
-    user_posts_path(current_user.id) # ログイン後に遷移するpathを設定
+    user_weddings_path(current_user.id) # ログイン後に遷移するpathを設定
   end
 
   def after_sign_out_path_for(resource)
