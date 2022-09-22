4cdcf5abb05c8c3e7f5a6c054347f7a5eb6533f5
rinooooo <rino.0566@gmail.com>
2022-09-22 19:24:50 +0900

    weddingモデルとchatモデルのアソシエーション

--- app/models/wedding.rb
+++ app/models/wedding.rb
@@ -2,5 +2,6 @@ class Wedding < ApplicationRecord
   belongs_to :user, optional: true
   has_many :posts
   has_many :communities
+  has_many :chats
   validates :gloom_name, :bride_name, :gloom_name_en, :bride_name_en, :date, :answer_date, :message, :location, :url, presence: true
 end
